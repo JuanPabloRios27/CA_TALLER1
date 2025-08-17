3de203d051aaa9f8435efa2541ca548eb1485f6d
JuanPabloRios27 <jprios@unbosque.edu.co>
2025-08-17 01:17:23 -0500

    Hace pruebas pero faltan el quicksort y mergesort de la matriz.

--- TallerCA1/src/co/edu/unbosque/model/Apoco.java
+++ TallerCA1/src/co/edu/unbosque/model/Apoco.java
@@ -6,62 +6,67 @@ import co.edu.unbosque.util.Matriz;
 import co.edu.unbosque.util.Ordenador;
 
 import java.lang.Math;
+
 /**
- * Aca almacenamos toda la información correspondiente con respecto al sistema de información que maneja la corporación APOCO.
+ * Aca almacenamos toda la información correspondiente con respecto al sistema
+ * de información que maneja la corporación APOCO.
+ * 
  * @author Alejandro
  * @author Juan Pablo Ríos Rodríguez.
  */
 public class Apoco {
 	/**
-	 * Un arreglo de longitud variable que implica todos los politicos que se encuentran bajo investigación.
+	 * Un arreglo de longitud variable que implica todos los politicos que se
+	 * encuentran bajo investigación.
 	 */
 	private Politico[] politicosSospechosos;
 	/**
 	 * Matriz de tamaño k*m que represente un grupo de n ladrones.
 	 */
 	private Ladron[][] ladrones;
+
 	/**
-	 * La lista de todas las pruebas empiricas que llevaron a cabo los algoritmos de ordenamiento.
-	 */
-	private List<Estadisticas> estadisticas;
-	/**
-	 * LLame a la función para realizar las funciones que otorga el arreglo de los políticos.
+	 * LLame a la función para realizar las funciones que otorga el arreglo de los
+	 * políticos.
 	 */
 	private Ordenador ordenador;
 	/**
 	 * LLame a la función para realizar los ejercicios de la propia matriz
 	 */
 	private Matriz matriz;
+
 	/**
 	 * Prepara las operaciones para realizar los respectivos algoritmos.
+	 * 
 	 * @author Juan Pablo Ríos Rodríguez.
 	 */
 	public Apoco() {
 		ordenador = new Ordenador();
 		matriz = new Matriz();
 	}
+
 	public String ordenarLadrones(int selectedIndex) {
 		StringBuilder politico = new StringBuilder();
-		if(selectedIndex == 0) {
-			ladrones =  matriz.bubbleSort(ladrones);
-			for(int i = 0; i < ladrones.length; i++) {
-				for(int j = 0; j < ladrones[0].length; j++) {
+		if (selectedIndex == 0) {
+			ladrones = matriz.bubbleSort(ladrones);
+			for (int i = 0; i < ladrones.length; i++) {
+				for (int j = 0; j < ladrones[0].length; j++) {
 					politico.append(ladrones[i][j].toString());
 				}
 				politico.append("\n");
 			}
-		}else if(selectedIndex == 1) {
-			ladrones =  matriz.selectionSort(ladrones);
-			for(int i = 0; i < ladrones.length; i++) {
-				for(int j = 0; j < ladrones[0].length; j++) {
+		} else if (selectedIndex == 1) {
+			ladrones = matriz.selectionSort(ladrones);
+			for (int i = 0; i < ladrones.length; i++) {
+				for (int j = 0; j < ladrones[0].length; j++) {
 					politico.append(ladrones[i][j].toString());
 				}
 				politico.append("\n");
 			}
-		}else if(selectedIndex == 2) {
-			ladrones =  matriz.insertionSort(ladrones);
-			for(int i = 0; i < ladrones.length; i++) {
-				for(int j = 0; j < ladrones[0].length; j++) {
+		} else if (selectedIndex == 2) {
+			ladrones = matriz.insertionSort(ladrones);
+			for (int i = 0; i < ladrones.length; i++) {
+				for (int j = 0; j < ladrones[0].length; j++) {
 					politico.append(ladrones[i][j].toString());
 				}
 				politico.append("\n");
@@ -69,89 +74,123 @@ public class Apoco {
 		}
 		return politico.toString();
 	}
+
 	/**
 	 * Muestra el arreglo con respecto a los políticos.
+	 * 
 	 * @author Juan Pablo Ríos Rodríguez
 	 * @return El arreglo completo con respecto a los politicos.
 	 */
 	public String mostrarPoliticos() {
 		StringBuilder politico = new StringBuilder();
-		for(int i = 0; i < politicosSospechosos.length; i++) {
+		for (int i = 0; i < politicosSospechosos.length; i++) {
 			politico.append(politicosSospechosos[i]);
 		}
 		return politico.toString();
 	}
+
 	/**
 	 * Imprime toda la matriz con respecto a los ladrones.
+	 * 
 	 * @author Juan Pablo Rios Rodriguez.
 	 * @return todos los ladrones registrados
 	 */
 	public String mostrarLadrones() {
 		StringBuilder ladron = new StringBuilder();
-		for(int i = 0; i < ladrones.length; i++) {
-			for(int j = 0; j < ladrones[0].length;j++) {
+		for (int i = 0; i < ladrones.length; i++) {
+			for (int j = 0; j < ladrones[0].length; j++) {
 				ladron.append(ladrones[i][j]);
 			}
 			ladron.append("\n");
 		}
 		return ladron.toString();
 	}
+
 	/**
-	 * Llame a la función para generar el arreglo con longitud n, que permita realizar la respectiva prueba.
+	 * Llame a la función para generar el arreglo con longitud n, que permita
+	 * realizar la respectiva prueba.
+	 * 
 	 * @author Juan Pablo Ríos Rodríguez
 	 */
 	public void generarPoliticos(int n) {
 		politicosSospechosos = ordenador.genereDatos(n);
 	}
+
 	public void generarLadrones(int int1) {
 		ladrones = matriz.genereMatriz(int1);
 	}
+
 	/**
-	 * Toma el arreglo de longitud variable que representa los politicos acusados de corrupción.
-	 * @return politicosSospechosos el arreglo de longitud variable que representa a los respectivos politicos acusados de corrupción.
+	 * Toma el arreglo de longitud variable que representa los politicos acusados de
+	 * corrupción.
+	 * 
+	 * @return politicosSospechosos el arreglo de longitud variable que representa a
+	 *         los respectivos politicos acusados de corrupción.
 	 */
 	public Politico[] getPoliticosSospechosos() {
 		return politicosSospechosos;
 	}
+
 	/**
 	 * Esta función obtiene la matriz que representa los ladrones.
-	 * @return ladrones La matriz de forma de arreglo que posiciona los respectivos 
+	 * 
+	 * @return ladrones La matriz de forma de arreglo que posiciona los respectivos
 	 */
 	public Ladron[][] getLadrones() {
 		return ladrones;
 	}
-	/**
-	 * Esta función obtiene las estadisticas.
-	 * @return estadisticas Las respectivas estadisticas que revelan resultados entorno al algoritmo que se ha probado de forma empirica. 
-	 */
-	public List<Estadisticas> getEstadisticas() {
-		return estadisticas;
-	}
+
 	/**
 	 * Modifique el arreglo de los politicos en caso de ser necesario.
+	 * 
 	 * @param politicosSospechosos El respectivo arreglo a modificar.
 	 */
 	public void setPoliticosSospechosos(Politico[] politicosSospechosos) {
 		this.politicosSospechosos = politicosSospechosos;
 	}
+
 	/**
 	 * Modifique la matriz que se encuentran almacenados los respectivos ladrones.
+	 * 
 	 * @param ladrones
 	 */
 	public void setLadrones(Ladron[][] ladrones) {
 		this.ladrones = ladrones;
 	}
-	/**
-	 * Modifique la lista de estadísticas.
-	 * @param estadisticas La lista de resultados que se va a modificar
-	 */
-	public void setEstadisticas(List<Estadisticas> estadisticas) {
-		this.estadisticas = estadisticas;
+
+	public String ordenarPoliticos(int selectedIndex) {
+		StringBuilder politico = new StringBuilder();
+		if (selectedIndex == 0) {
+			politicosSospechosos = ordenador.bubbleSort(politicosSospechosos);
+			for (int i = 0; i < politicosSospechosos.length; i++) {
+				politico.append(politicosSospechosos[i].toString());
+			}
+		} else if (selectedIndex == 1) {
+			politicosSospechosos = ordenador.selectionSort(politicosSospechosos);
+			for (int i = 0; i < politicosSospechosos.length; i++) {
+				politico.append(politicosSospechosos[i].toString());
+			}
+			return politico.toString();
+		} else if (selectedIndex == 2) {
+			politicosSospechosos = ordenador.insertionSort(politicosSospechosos);
+			for (int i = 0; i < politicosSospechosos.length; i++) {
+				politico.append(politicosSospechosos[i].toString());
+			}
+			return politico.toString();
+		} else if (selectedIndex == 3) {
+			politicosSospechosos = ordenador.mergeSort(politicosSospechosos, 0, politicosSospechosos.length-1);
+			for (int i = 0; i < politicosSospechosos.length; i++) {
+				politico.append(politicosSospechosos[i].toString());
+			}
+			return politico.toString();
+		}else if (selectedIndex == 4) {
+			politicosSospechosos = ordenador.quickSort(politicosSospechosos, 0, politicosSospechosos.length-1);
+			for (int i = 0; i < politicosSospechosos.length; i++) {
+				politico.append(politicosSospechosos[i].toString());
+			}
+			return politico.toString();
+		}
+		return politico.toString();
 	}
-	
-	
-	
-	
-	
-	
+
 }
--- TallerCA1/src/co/edu/unbosque/util/Ordenador.java
+++ TallerCA1/src/co/edu/unbosque/util/Ordenador.java
@@ -1,10 +1,13 @@
 package co.edu.unbosque.util;
 
+import co.edu.unbosque.controller.Controller;
+import co.edu.unbosque.model.Estadisticas;
 import co.edu.unbosque.model.Ladron;
 import co.edu.unbosque.model.Politico;
+import co.edu.unbosque.view.Gui;
 
 public class Ordenador {
-	
+	Estadisticas estadisticas = new Estadisticas();
 	/**
 	 * Permite generar datos con respecto a la matriz.
 	 * @author Juan Pablo Ríos Rodríguez.
@@ -17,24 +20,179 @@ public class Ordenador {
 		return politicosSospechosos;
 	}
 	
-	public Politico[] bubbleSort() {
-		return null;
+	public Politico[] bubbleSort(Politico[] politicos) {
+		Estadisticas estadisticas = new Estadisticas();
+		estadisticas.setAlgoritmo("InsertionSort");
+		estadisticas.captureTiempo();
+		Politico temp;
+		for(int i = 0; i < politicos.length - 1; i++) {
+			for(int j = 0; j < politicos.length - i -1 ; j++) {
+				estadisticas.setComparaciones(estadisticas.getComparaciones()+1);
+				if(politicos[j].getDineroRobado() > politicos[j+1].getDineroRobado()) {
+					temp = politicos[j];
+					politicos[j] = politicos[j +1];
+					politicos[j+1] = temp;
+					estadisticas.setInteraciones(estadisticas.getInteraciones());
+				}
+			}
+		}
+		estadisticas.finalizeTiempo();
+		Controller.imprimeEstadisticas(estadisticas);
+		return politicos;
 	}
 	
-	public Politico[] selectionSort() {
-		return null;
+	public Politico[] selectionSort(Politico[] politico) {
+		Estadisticas estadisticas = new Estadisticas();
+		estadisticas.setAlgoritmo("InsertionSort");
+		estadisticas.captureTiempo();
+		int n = politico.length;
+		for (int i = 0; i < n - 1; i++) {
+			int min_idx = i;
+			for(int j = i + 1; j < n; j++) {
+				estadisticas.setComparaciones(estadisticas.getComparaciones()+1);
+				if(politico[j].getDineroRobado() < politico[min_idx].getDineroRobado()) {
+					min_idx = j;
+				}
+			}
+			if(min_idx != i) {
+				Politico temp = politico[i];
+				politico[i] = politico[min_idx];
+				politico[min_idx] = temp;
+				estadisticas.setInteraciones(estadisticas.getInteraciones()+1);
+			}
+		}
+		estadisticas.finalizeTiempo();
+		Controller.imprimeEstadisticas(estadisticas);
+		return politico;
+	}
+	public void merge(Politico[] politico, int left, int mid, int right) {
+		int n1 = mid - left + 1;
+	    int n2 = right - mid;
+	    //Crea dos arreglos temporales.
+	    Politico L[] = new Politico[n1];
+	    Politico R[] = new Politico[n2];
+	    for (int i = 0; i <n1; i++) {
+	    	L[i] = politico[left + i];
+	    }
+	    for(int j = 0; j < n2; j++) {
+	    	R[j] = politico[mid +1 + j];
+	    }
+	    int i = 0, j = 0;
+	    int k = left;
+	    while(i < n1 && j < n2) {
+	    	estadisticas.setComparaciones(estadisticas.getComparaciones()+1);
+	    	if(L[i].getDineroRobado() <= R[j].getDineroRobado()) {
+	    		politico[k] = L[i];
+	    		i++;
+	    	}else {
+	    		politico[k] = R[j];
+	    		j++;
+	    	}
+	    	estadisticas.setInteraciones(estadisticas.getInteraciones()+1);
+	    	k++;
+	    }
+	    while(i < n1) {
+	    	politico[k] = L[i];
+	    	i++;
+	    	k++;
+	    	estadisticas.setInteraciones(estadisticas.getInteraciones()+1);
+	    }
+	    while(j < n2) {
+	    	politico[k] = R[j];
+	    	j++;
+	    	k++;
+	    	estadisticas.setInteraciones(estadisticas.getInteraciones()+1);
+	    }
+	}
+	public Politico[] insertionSort(Politico[] politico) {
+		Estadisticas estadisticas = new Estadisticas();
+		estadisticas.setAlgoritmo("InsertionSort");
+		estadisticas.captureTiempo();
+		int n = politico.length;
+		for (int i = 1; i < n; i++) {
+			Politico key = politico[i];
+			int j = i - 1;
+			while(j>=0) {
+				estadisticas.setComparaciones(estadisticas.getComparaciones()+1);
+				if(politico[j].getDineroRobado() > key.getDineroRobado()) {
+					politico[j + 1] = politico[j];
+					estadisticas.setInteraciones(estadisticas.getInteraciones()+1);
+					j--;
+				}else {
+					break;
+				}
+			}
+			politico[j + 1] = key;
+		}
+		estadisticas.finalizeTiempo();
+		Controller.imprimeEstadisticas(estadisticas);
+		return politico;
 	}
 	
-	public Politico[] insertionSort() {
-		return null;
+	public Politico[] mergeSort(Politico[] politico, int left, int right) {
+		estadisticas = new Estadisticas();
+		estadisticas.setAlgoritmo("InsertionSort");
+		estadisticas.captureTiempo();
+		if(left < right) {
+			int mid = left + (right - left) / 2;
+			mergeSort(politico, left, mid);
+			mergeSort(politico, mid + 1, right);
+			merge(politico, left, mid, right);
+		}
+		estadisticas.finalizeTiempo();
+		Controller.imprimeEstadisticas(estadisticas);
+		return politico;
 	}
-	
-	public Politico[] mergeSort() {
-		return null;
+	/**
+	 * Implementación del respectivo QuickSort.
+	 * Se implemento usando la partición de Lomuto.
+	 * @param politico
+	 * @param left
+	 * @param right
+	 * @return
+	 */
+	public Politico[] quickSort(Politico[] politico, int left, int right) {
+		estadisticas = new Estadisticas();
+		estadisticas.setAlgoritmo("InsertionSort");
+		estadisticas.captureTiempo();
+		if(left < right) {
+			int pi = partition(politico, left, right);
+			quickSort(politico, left, pi -1);
+			quickSort(politico, pi+1, right);
+		}
+		estadisticas.finalizeTiempo();
+		Controller.imprimeEstadisticas(estadisticas);
+		return politico;
 	}
-	
-	public Politico[] quickSort() {
-		return null;
+	/**
+	 * Elija el pivote y haga particiones.
+	 * @param politico
+	 * @param left el elemento que se va a comparar con el siguiente.
+	 * @param right el elemento que esta delante del anterior.
+	 * @return La partición del arreglo.
+	 */
+	private int partition(Politico[] politico, int left, int right) {
+		Politico pivot = politico[right];
+		int i = left -1;
+		for(int j = left; j <= right -1; j++) {
+			if(politico[j].getDineroRobado() < pivot.getDineroRobado()) {
+				i++;
+				swap(politico, i, j);
+			}
+		}
+		swap(politico, i+1, right);
+		return i + 1;
+	}
+	/**
+	 * Cambie los valores de acuerdo con QuickSort.
+	 * @param politico
+	 * @param i Intercambia el elemento que es mayor con el que esta adelante.
+	 * @param j El elemento a intercambiar.
+	 */
+	private void swap(Politico[] politico, int i, int j) {
+		Politico temp = politico[i];
+		politico[i] = politico[j];
+		politico[j] = temp;
 	}
 	
 }
